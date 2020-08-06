c141f0c7d2c4e58c30f747d9f120665ae2136cfa
unknown <mtelaprolu@gmail.com>
2020-08-06 17:21:42 -0400

    Commiting the changes made to LoginTest

--- src/test/java/loginPages/LoginTest.java
+++ src/test/java/loginPages/LoginTest.java
@@ -3,6 +3,7 @@ package loginPages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -21,6 +22,20 @@ public class LoginTest {
 		driver.findElement(By.id("user")).sendKeys("sukkumar3");
 		driver.findElement(By.id("pass")).sendKeys("selenium@2020");
 		driver.findElement(By.name("btnSubmit")).click();
+		Reporter.log("credentials submitted");
+		String actMsg = driver.findElement(By.tagName("h5")).getText();
+		String expMsg = "Welcome sukkumar3";
+
+		if (actMsg.equals(expMsg)) {
+			System.out.println("test case pass");
+
+		} else {
+			System.out.println("test case fail");
+
+		}
+		Reporter.log("verify the expected message");
+		System.out.println("test completed");
+		Reporter.log("test completed");
 	}
 	@AfterSuite
 	public void teatDown() {
